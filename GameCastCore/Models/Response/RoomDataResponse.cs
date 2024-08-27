348d5d6c27518a7fe043370046d7eefdd1195934
Niklas Vlach <contact@niklas-vlach.com>
2024-08-27 20:37:09 +0200

    removed constructor

--- GameCastCore/Models/Response/RoomDataResponse.cs
+++ GameCastCore/Models/Response/RoomDataResponse.cs
@@ -4,27 +4,14 @@ namespace GameCast.Core.Models.Response;
 
 public class RoomDataResponse
 {
-    [JsonPropertyName("code")] public string Code { get; private set; }
+    [JsonPropertyName("code")] public string Code { get; set; }
 
-    [JsonPropertyName("appId")] public string ApplicationId { get; private set; }
-    [JsonPropertyName("appTag")] public string ApplicationTag { get; private set; }
-    [JsonPropertyName("minPlayers")] public int MinPlayers { get; private set; }
-    [JsonPropertyName("maxPlayers")] public int MaxPlayers { get; private set; }
+    [JsonPropertyName("appId")] public string ApplicationId { get; set; }
+    [JsonPropertyName("appTag")] public string ApplicationTag { get; set; }
+    [JsonPropertyName("minPlayers")] public int MinPlayers { get; set; }
+    [JsonPropertyName("maxPlayers")] public int MaxPlayers { get; set; }
 
-    [JsonPropertyName("server")] public string Server { get; private set; }
+    [JsonPropertyName("server")] public string Server { get; set; }
 
-    [JsonPropertyName("full")] public bool IsFull { get; private set; }
-
-    public RoomDataResponse(string roomCode, string appId, string appTag, int minPlayers, int maxPlayers,
-        string server, bool isFull)
-    {
-        Code = roomCode;
-        ApplicationId = appId;
-        ApplicationTag = appTag;
-        MinPlayers = minPlayers;
-        MaxPlayers = maxPlayers;
-        Server = server;
-        IsFull = isFull;
-    }
-    
+    [JsonPropertyName("full")] public bool IsFull { get; set; }
 }
